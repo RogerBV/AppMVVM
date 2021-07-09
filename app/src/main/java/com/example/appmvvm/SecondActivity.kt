75645e75b17e30f816aaa01a45064f140c3fd0f8
Roger Barriga <rogerbv_9017@hotmail.com>
2021-07-09 08:17:06 -0500

    Implement RecyclerView Adapter

--- app/src/main/java/com/example/appmvvm/SecondActivity.kt
+++ app/src/main/java/com/example/appmvvm/SecondActivity.kt
@@ -4,16 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.example.appmvvm.viewmodels.PracticeViewModel
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class SecondActivity : AppCompatActivity() {
     lateinit var practiceViewModel: PracticeViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContentView(R.layout.activity_main)
 
         //fun list CoroutineScope.async { ListPermits() }
         GlobalScope.launch {
---
+++ app/src/main/java/com/example/appmvvm/adapters/PermitSearchResultsAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.appmvvm.adapters
+
+import android.R
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appmvvm.models.RegisteredPermit
+import okhttp3.internal.Util
+import androidx.appcompat.app.AppCompatActivity
+class PermitSearchResultsAdapter:
+    RecyclerView.Adapter<PermitSearchResultsAdapter.PermitSearchResultHolder>() {
+    private var results: List<RegisteredPermit> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PermitSearchResultHolder {
+        val itemView: View = LayoutInflater.from(parent.context)
+            .inflate(com.example.appmvvm.R.layout.permit_item  , parent, false)
+        return PermitSearchResultHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: PermitSearchResultHolder, position: Int) {
+        val permit: RegisteredPermit = results[position]
+
+        if (permit.EmployeeName!=null)
+        {
+            holder.employeeNameTextView.text = permit.EmployeeName
+        }
+
+        if(permit.EmployeeSurname != null)
+        {
+            holder.employeeSurnameTextView.text = permit.EmployeeSurname
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return results.size
+    }
+
+    fun setResults(results: List<RegisteredPermit>) {
+        this.results = results
+        notifyDataSetChanged()
+    }
+
+    class PermitSearchResultHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        lateinit var employeeNameTextView: TextView
+        lateinit var employeeSurnameTextView: TextView
+
+
+        init {
+            employeeNameTextView = itemView.findViewById(com.example.appmvvm.R.id.permit_employee_name)
+            employeeSurnameTextView = itemView.findViewById(com.example.appmvvm.R.id.permit_employee_surname)
+
+        }
+    }
+
+}
